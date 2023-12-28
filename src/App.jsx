63630b5a1e3cc88cf57c9a48a29ace5655ad8f1e
Juanma <juanmagp26@gmail.com>
2023-12-28 21:08:44 +0100

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -25,14 +25,14 @@ function App() {
         <div className="flex-1">
 
       <h1 className='text-6xl font-bold text-center mt-28 font-poppins text-violet-800'>Todo Machine</h1>
-      <h2 className="text-2xl mt-4 text-violet-700">Has completado 0 de 0 tareas</h2>
-      <div className="flex items-center mt-4">
+      <h2 className="text-2xl mt-4 text-center text-violet-700">Has completado 0 de 0 tareas</h2>
+      <div className="flex flex-col items-center mt-4">
         
-        <div className='relative mt-4'>
+        <div className='relative w-full md:w-96 mt-4'>
           <input
           type="text"
           placeholder="Buscar..."
-          className="py-2 px-4 pr-10 w-full md:w-96 border border-gray-300 rounded-md focus:outline-none focus:border-blue-400"
+          className="py-2 px-4 mx-auto items-center pr-10 w-full md:w-96 border border-solid bg-orange-200 border-gray-300 focus:outline-2 shadow-2xl rounded-xl"
         />
         <span className="absolute inset-y-0 right-0 flex items-center px-3">
         <FontAwesomeIcon icon={faSearch} className="text-gray-400" />
@@ -41,8 +41,9 @@ function App() {
         </div>
         
       </div>
-      <h2 className="text-1xl mt-6 text-violet-700">Crea tu primera tarea!</h2>
-
+      <div className='flex-1'>
+      <h2 className="text-1xl mt-6 text-center text-violet-700">Crea tu primera tarea!</h2>
+</div>
     </div>
     </div>
     </div>
